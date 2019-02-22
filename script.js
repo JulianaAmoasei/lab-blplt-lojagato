1a922f0ebf2819f82222e0ab7146eac32466c48c
JulianaAmoasei <late.parrot@gmail.com>
2019-02-22 03:02:58 -0300

    boilerplate do exercicio finalizado

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  // alert("carregou");
+  showProducts();
+
+};
+
+function getProducts(){
+}
+
+function showProducts(){
+
+}
+
+// const btnSubmit = document.getElementById("btn-submit");
+
+// btnSubmit.addEventListener("click", function(e){
+
+// });
+
+// function resetForm(form){
+
+// }
